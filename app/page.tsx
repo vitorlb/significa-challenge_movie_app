51877c71091811c83951196cef09e9d6d9030058
vitorlb <vitorlinhabranco@gmail.com>
2024-06-12 09:50:27 +0100

    add node version to package

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import PageFrame from "./components/pageFrame";
 import ThumbList from "./components/thumbList";
 export default function Home() {
